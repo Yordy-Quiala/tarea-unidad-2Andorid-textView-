88a4b008aa3328114dddc8be61ebc892047940a1
Yordy <yordyquiala92@gmail.com>
2023-03-25 09:52:01 +0100

    primer commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+       // helloTextView.setText(R.string.user_greeting);
+    }
+}
